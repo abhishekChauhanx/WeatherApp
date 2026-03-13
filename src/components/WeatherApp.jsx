04c5d1291109df509cd3a0b092ded169444d7d9f
abhishekChauhanx <rajputabhic12@gmail.com>
2026-03-13 22:21:27 +0530

    Update

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -20,7 +20,7 @@ const WeatherApp = () => {
     try {
       const response = await fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`
-      );
+      );          
 
       if (!response.ok) {
         throw new Error("Weather data not available");
